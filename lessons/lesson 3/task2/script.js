6acc7d46d1257fe6cbc686b92ca4b0155174faf5
Ivan <132990987+Shakro001@users.noreply.github.com>
2024-08-03 10:00:10 +0000

    Delete lessons directory

--- lessons/lesson 3/task2/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-for (let i = 3; i <= 8; i++) {
-    if (i <= 5){
-        document.write(`spring month ${i} <br>`)
-    } else{
-        document.write(`summer month ${i} <br>`)
-    }
-}
--- lessons/lesson 3/task5/script.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-
-for( let i = 0; i<10; i++){
-    document.write(`
-        product #${i+1} <input type='text'> <br>
-        `)
-}
